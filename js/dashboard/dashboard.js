40763f7f3294e09ef36194999c338f86699e486b
Ricardo T <ricardo@nucliux.mx>
2023-10-10 10:02:44 -0600

    vista de temas en dashboard

--- js/dashboard/dashboard.js
+++ js/dashboard/dashboard.js
@@ -2,6 +2,7 @@ const perfil = document.querySelector("#sesionCorreo");
 perfil.innerHTML = `Hola ${sessionStorage.getItem("usuarioCorreo")??""}!`;
 
 let listaFrasesGlobal = [];
+let listaTemasGlobal = [];
 
 recibirFrases();
 
@@ -9,19 +10,37 @@ recibirFrases();
 function recibirFrases() {
 	
 	let peticionFrases = new XMLHttpRequest();
-
 	peticionFrases.open('GET', "../jsons/frases.json", false); 
 	peticionFrases.send(null);
 	listaFrasesGlobal = JSON.parse(peticionFrases.responseText);
 
+	let peticionTemas = new XMLHttpRequest();
+	peticionTemas.open('GET', "../jsons/temas.json", false); 
+	peticionTemas.send(null);
+	listaTemasGlobal = JSON.parse(peticionTemas.responseText);
+
+	/**Llenar los select de formularios con temas disponibles*/
+	let selectOpciones = `<option value="0">-Selecciona-</option>`;
+
+	listaTemasGlobal.forEach(item => {
+		selectOpciones += `<option value="${item.id}">${item.tema}</option>`;
+	});
+
+	const selectCreaTema = document.querySelector("#creaTema");
+	selectCreaTema.innerHTML = selectOpciones;
+
+	const selectActualizaTema = document.querySelector("#actualizaTema");
+	selectActualizaTema.innerHTML = selectOpciones;
+
+	/**Llenar la tabla de datos */
 	let tablaHTML = `
 		<thead>
 			<tr>
-				<th class="text-right">Id</th>
+				<th class="tabla-columna-comprime text-right">Id</th>
 				<th>Frase</th>
 				<th>Autor</th>
 				<th class="text-right">Tema</th>
-				<th class="text-center">Opciones</th>
+				<th class="tabla-columna-comprime text-center">Opciones</th>
 			<tr>
 		</thead>
 		<tbody>`;
@@ -29,13 +48,13 @@ function recibirFrases() {
 		listaFrasesGlobal.forEach(fraseItem => {
 		tablaHTML += `
 			<tr>
-				<td class="text-right">${fraseItem.id}</td>
+				<td class="tabla-columna-comprime text-right">${fraseItem.id}</td>
 				<td>${fraseItem.frase}</td>
 				<td>${fraseItem.autor}</td>
-				<td class="text-right">${fraseItem.tema}</td>
-				<td class="text-center">
+				<td class="text-right">${listaTemasGlobal.find(item => fraseItem.tema == item.id).tema}</td>
+				<td class="tabla-columna-comprime text-center">
 					<button type="button" onclick="actualizaFraseMostrar(${fraseItem.id})">Editar</button>
-					<button type="button">Eliminar</button>
+					<button type="button" onclick="eliminaFraseMostrar(${fraseItem.id})" class="boton boton-red">Eliminar</button>
 				</td>
 			</tr>`;
 	});
@@ -43,14 +62,26 @@ function recibirFrases() {
 	tablaHTML += `</tbody>`;
 
 	const tablaElemento = document.querySelector("#tablaFrases");
-
 	tablaElemento.innerHTML = tablaHTML;
 }
 
+
 function creaFraseMostrar() {
+	const texto = document.querySelector("#creaFrase");
+	texto.value = "";
+	const autor = document.querySelector("#creaAutor");
+	autor.value = "";
+	const tema = document.querySelector("#creaTema");
+	tema.value = 0;
+
 	const modal = document.querySelector("#creaFraseModal");
 	modal.style.display = 'flex';
 }
+
+function creaFraseValidar() {
+	cierraModal();
+}
+
 
 function actualizaFraseMostrar(id) {
 	const item = listaFrasesGlobal.find(item => item.id == id);
@@ -66,6 +97,25 @@ function actualizaFraseMostrar(id) {
 	modal.style.display = 'flex';
 }
 
+function actualizaFraseValidar() {
+	cierraModal();
+}
+
+
+function eliminaFraseMostrar(id) {
+	const item = listaFrasesGlobal.find(item => item.id == id);
+	
+	const texto = document.querySelector("#eliminaFrase");
+	texto.innerHTML = '"'+item.frase+'"';
+
+	const modal = document.querySelector("#eliminaFraseModal");
+	modal.style.display = 'flex';
+}
+
+function eliminaFraseAccion() {
+	cierraModal();
+}
+
 
 function cierraModal() {
 	const modales = document.querySelectorAll(".modal");
@@ -73,6 +123,7 @@ function cierraModal() {
 		item.style.display = "none";
 	});
 }
+
 
 /**Función para salir de la sesión y limpiar los datos almacenados */
 function cerrarSesion() {
---
+++ js/dashboard/temas.js
@@ -0,0 +1,101 @@
+const perfil = document.querySelector("#sesionCorreo");
+perfil.innerHTML = `Hola ${sessionStorage.getItem("usuarioCorreo")??""}!`;
+
+let listaTemasGlobal = [];
+
+recibirTemas();
+
+/**Función para llenar la tabla de temas con datos */
+function recibirTemas() {
+	
+	let peticionTemas = new XMLHttpRequest();
+
+	peticionTemas.open('GET', "../jsons/temas.json", false); 
+	peticionTemas.send(null);
+	listaTemasGlobal = JSON.parse(peticionTemas.responseText);
+
+	let tablaHTML = `
+		<thead>
+			<tr>
+				<th class="tabla-columna-comprime text-right">Id</th>
+				<th>Tema</th>
+				<th class="tabla-columna-comprime text-center">Opciones</th>
+			<tr>
+		</thead>
+		<tbody>`;
+
+		listaTemasGlobal.forEach(temaItem => {
+		tablaHTML += `
+			<tr>
+				<td class="tabla-columna-comprime text-right">${temaItem.id}</td>
+				<td>${temaItem.tema}</td>
+				<td class="tabla-columna-comprime text-center">
+					<button type="button" onclick="actualizaTemaMostrar(${temaItem.id})">Editar</button>
+					<button type="button" onclick="eliminaTemaMostrar(${temaItem.id})" class="boton boton-red">Eliminar</button>
+				</td>
+			</tr>`;
+	});
+
+	tablaHTML += `</tbody>`;
+
+	const tablaElemento = document.querySelector("#tablaTemas");
+
+	tablaElemento.innerHTML = tablaHTML;
+}
+
+function creaTemaMostrar() {
+	const modal = document.querySelector("#creaTemaModal");
+	modal.style.display = 'flex';
+}
+
+function creaTemaValidar() {
+	cierraModal();
+}
+
+
+function actualizaTemaMostrar(id) {
+	const item = listaTemasGlobal.find(item => item.id == id);
+	
+	const texto = document.querySelector("#actualizaTema");
+	texto.value = item.tema;
+
+	const modal = document.querySelector("#actualizaTemaModal");
+	modal.style.display = 'flex';
+}
+
+function actualizaTemaValidar() {
+	cierraModal();
+}
+
+
+function eliminaTemaMostrar(id) {
+	const item = listaTemasGlobal.find(item => item.id == id);
+	
+	const texto = document.querySelector("#eliminaTema");
+	texto.innerHTML = '"'+item.tema+'"';
+
+	const modal = document.querySelector("#eliminaTemaModal");
+	modal.style.display = 'flex';
+}
+
+function eliminaTemaAccion() {
+	cierraModal();
+}
+
+
+function cierraModal() {
+	const modales = document.querySelectorAll(".modal");
+	modales.forEach(item => {
+		item.style.display = "none";
+	});
+}
+
+
+/**Función para salir de la sesión y limpiar los datos almacenados */
+function cerrarSesion() {
+	
+	sessionStorage.clear();
+	
+	window.location.href = "index.html";
+	
+}
